7064161f61f2ab26661f7fc2e01cbc6405a5e25d
unknown <carlosruanaraujo789@gmail.com>
2023-02-06 08:42:19 -0300

    faltando alguns retoques de responsivo

--- FrontBanzaiflix/src/components/Main.js
+++ FrontBanzaiflix/src/components/Main.js
@@ -2,31 +2,27 @@ import React from "react";
 
 function Main() {
   return (
-    <div className="d-flex  flex-column justify-content-center principal">
-      <img
-        className="img-fluid w-25"
-        src="https://fontmeme.com/permalink/221218/1df59e3f82fc338190d882577fa9adf6.png"
-        alt="fonte-de-stranger-things"
-        border="0"
-      />
-      <h4 className="m-3 col-sm-10 col-lg-10 col-md-10 col-xl-8 col-xxl-5 text-white">
-        Quando uma garota desaparece, a cidade toda participa nas buscas. Mas o
-        que encontram são segredos, forças sobrenaturais e uma menina
-      </h4>
-      <div className="d-flex m-3">
-        <button className="  btn btn-light btn-main text-capitalize">
-          Assistir
-        </button>
-        <button className="ms-3 btn btn-light btn-main text-capitalize">
-          Saiba Mais!
-        </button>
+    <div className="principal">
+      <div className="container-main d-flex flex-column">
+        <img
+          className="StrangerThings-F"
+          src="https://fontmeme.com/permalink/221218/1df59e3f82fc338190d882577fa9adf6.png"
+          alt="fonte-de-stranger-things"
+          border="0px"
+        />
+        <h4 className="m-3 col-sm-10 col-lg-10 col-md-10 col-xl-8 col-xxl-5 text-white">
+          Quando uma garota desaparece, a cidade toda participa nas buscas. Mas
+          o que encontram são segredos, forças sobrenaturais e uma menina
+        </h4>
+        <div className="d-flex m-3">
+          <button className="  btn p-4 d-flex flex-wrap justify-content-center align-content-center btn-light btn-main text-capitalize">
+            Assistir
+          </button>
+          <button className="ms-3 d-flex flex-wrap p-4 align-content-center justify-content-center btn btn-light btn-main text-capitalize">
+            Saiba Mais!
+          </button>
+        </div>
       </div>
-      <h1
-        className="text-white ms-5"
-        style={{ position: "absolute", marginTop: "950px" }}
-      >
-        Em Alta
-      </h1>
     </div>
   );
 }
